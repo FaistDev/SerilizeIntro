977f3e1cc0a065d43d51ce1dc4232b44d1b4929a
FaistDev <ben@faistdev.at>
2018-09-26 12:58:01 +0200

    Added testcode

--- SerilizeIntro/src/SchuelerBL.java
+++ SerilizeIntro/src/SchuelerBL.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 
 /*
@@ -48,6 +49,18 @@ public class SchuelerBL {
     }
     
     public static void main(String[] args) {
+        Schueler s1 = new Schueler("Hans", LocalDate.of(2000, Month.MARCH, 3));
+        Schueler s2 = new Schueler("Grete", LocalDate.of(1999, Month.OCTOBER, 10));
         
+        SchuelerBL bl = new SchuelerBL();
+        bl.add(s1);
+        bl.add(s2);
+        
+        File f = new File("./data.csv");
+        try{
+            bl.save(f);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
