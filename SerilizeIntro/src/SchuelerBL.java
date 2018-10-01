7aea031c80b218e61af06433eb3247c1f939c61e
FaistDev <ben@faistdev.at>
2018-10-01 10:52:42 +0200

    Tested load

--- SerilizeIntro/src/SchuelerBL.java
+++ SerilizeIntro/src/SchuelerBL.java
@@ -17,51 +17,53 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Ben
  */
 public class SchuelerBL {
+
     private ArrayList<Schueler> schueler = new ArrayList<>();
-    
-    public void add(Schueler s){
+
+    public void add(Schueler s) {
         schueler.add(s);
     }
-    
-    public void save(File f) throws Exception{
+
+    public void save(File f) throws Exception {
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
         for (Schueler s : schueler) {
             oos.writeObject(s);
         }
         oos.flush();
     }
-    
-    public void load(File f) throws Exception{
+
+    public void load(File f) throws Exception {
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
-       Object s=null;
-        while((s=ois.readObject())!=null){
-            try{
+        Object s = null;
+        while ((s = ois.readObject()) != null) {
+            try {
                 add((Schueler) s);
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-            
+
         }
     }
-    
+
     public static void main(String[] args) {
         Schueler s1 = new Schueler("Hans", LocalDate.of(2000, Month.MARCH, 3));
         Schueler s2 = new Schueler("Grete", LocalDate.of(1999, Month.OCTOBER, 10));
-        
+
         SchuelerBL bl = new SchuelerBL();
         bl.add(s1);
         bl.add(s2);
-        
+
         File f = new File("./data.bin");
-        try{
+        try {
             bl.save(f);
-        }catch(Exception e){
+
+            bl.load(f);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
